7a258f5b14b41e35df5d0051b3cf9dfda207b7d1
to4ka2225 <to4ka5464@gmail.com>
2026-09-26 15:28:12 +0300

    Add files via upload

--- assets/main.py
+++ assets/main.py
@@ -3,12 +3,64 @@ from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.screen import MDScreen
 from kivy import platform
 from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.uix.image import Image
+
+
+class Bullet(Image):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.source = "assets/images/bullet.png"
+        self.size_hint = (None, None)
+        self.size = (10, 30)
+
 
 class MainScreen(MDScreen):
     ...
 
+
 class GameScreen(MDScreen):
-    ...
+    fps = 60
+    ship_speed = 5
+    bullet_speed = 10
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.keys = {"left": False, "right": False, "fire": False}
+        self.bullets = []
+        self.game_event = None
+
+    def on_enter(self, *args):
+        self.game_event = Clock.schedule_interval(self.update, 1 / self.fps)
+
+    def on_leave(self, *args):
+        if self.game_event:
+            self.game_event.cancel()
+
+    def update(self, dt):
+        ship = self.ids.ship
+
+        if self.keys["left"] and ship.x > 0:
+            ship.x -= self.ship_speed
+
+        if self.keys["right"] and ship.right < Window.width:
+            ship.x += self.ship_speed
+
+        for bullet in self.bullets[:]:
+            bullet.y += self.bullet_speed
+            if bullet.y > Window.height:
+                self.ids.front.remove_widget(bullet)
+                self.bullets.remove(bullet)
+
+    def fire(self):
+        bullet = Bullet()
+
+        bullet.center_x = self.ids.ship.center_x
+        bullet.y = self.ids.ship.top
+
+        self.ids.front.add_widget(bullet)
+        self.bullets.append(bullet)
+
 
 class ShooterApp(MDApp):
     def build(self):
@@ -22,8 +74,9 @@ class ShooterApp(MDApp):
 
         return self.sm
 
+
 if platform != "android":
-    Window.size = (400,900)
+    Window.size = (400, 900)
     Window.top = 100
     Window.left = 600
 
